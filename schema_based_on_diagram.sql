5bbf491cfc73c0890874fa3a458143c940fc03cd
dlwub <dlwubi@gmail.com>
2022-11-02 18:52:23 +0300

    Add foreign indexes

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -9,6 +9,7 @@ CREATE TABLE medical_histories (
 	admitted_at TIMESTAMP,   
 	patient_id INT,
 	status varchar(120),
+	FOREIGN KEY patient_id REFERENCES patient(id),
 );
 
 CREATE TABLE invoices (
@@ -32,11 +33,19 @@ CREATE TABLE invoice_items (
 	quantity INT,
 	total_price DECIMAL,
 	invoice_id INT,
-	treatment_id INT,  
+	treatment_id INT,
+	FOREIGN KEY (invoice_id) REFERENCES invoices(id);  
 	FOREIGN KEY (treatment_id) REFERENCES treatments(id);
 );
 
 CREATE TABLE treatment_history (
     treatments_id INT REFERENCES treatments (id),
     medical_history_id INT REFERENCES medical_histories (id)
-)
+);
+
+/* Foreign key indexes */
+
+CREATE INDEX patient_id_asc ON medical_histories(patient_id ASC);
+CREATE INDEX medical_history_id_asc ON invoices(medical_history_id ASC);
+CREATE INDEX invoice_treatment_DESC ON invoice_items(invoice_id DESC, treatment_id DESC);
+CREATE INDEX treatment_history_ASC ON treatment_history(treatments_id ASC, medical_history_id ASC);
